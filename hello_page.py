5e9ad74ed766233c5371f833f655fa71da220000
unknown <sebastian.konicz@gmail.com>
2023-12-06 23:18:19 +0100

    Initial commit with structure of a project

---
+++ hello_page.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import os
+
+st.set_page_config(
+    page_title="Strava HR Zones",
+    page_icon='heart',
+    )
+
+
+st.markdown(
+    """
+    ### :heart: :running: Streamlit Strava HR Zones Viever :bicyclist: :heart:
+    This is a proof of concept of a [Streamlit](https://streamlit.io/) application that implements the [Strava API](https://developers.strava.com/) OAuth2 authentication flow.
+    The source code can be found at [my GitHub](https://github.com/sebastian-konicz/strava-hr-zones) and is licensed under an [MIT license](https://github.com/sebastian-konicz/strava-hr-zones/blob/main/LICENSE).
+
+    [Contact me](sebastian.konicz@gmail.com) if you have any suggestions.
+    """
+)
