292991389fdbcdc52b353a722498cf1d7c8e3603
unknown <sebastian.konicz@gmail.com>
2023-12-12 19:28:22 +0100

    Changed pandas verstion to 1.5.3 to be compliant

--- hello_page.py
+++ hello_page.py
@@ -15,7 +15,7 @@ st.markdown(
     This is a proof of concept of a [Streamlit](https://streamlit.io/) application that implements the [Strava API](https://developers.strava.com/) OAuth2 authentication flow.
     The source code can be found at [my GitHub](https://github.com/sebastian-konicz/strava-hr-zones) and is licensed under an [MIT license](https://github.com/sebastian-konicz/strava-hr-zones/blob/main/LICENSE).
 
-    [Contact me](sebastian.konicz@gmail.com) if you have any suggestions.
+    Contact me if you have any suggestions: sebastian.konicz@gmail.com.
     
     Project inspired and partialy based on project of Aart Goossens  [GitHub](https://github.com/AartGoossens/streamlit-activity-viewer)
     """
